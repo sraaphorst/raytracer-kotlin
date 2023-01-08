e1cc97f1baf7b61e9853e098ce07765d8171f2dc
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-01-08 15:22:06 -0500

    Chapter 9 completed.

---
+++ src/test/kotlin/shapes/planetest.kt
@@ -0,0 +1,56 @@
+package shapes
+
+// By Sebastian Raaphorst, 2023.
+
+import math.Ray
+import math.Tuple
+import math.assertAlmostEquals
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class PlaneTest {
+    @Test
+    fun `Normal of plane is constant everywhere`() {
+        val p = Plane()
+        assertAlmostEquals(Tuple.VY, p.localNormalAt(Tuple.PZERO))
+        assertAlmostEquals(Tuple.VY, p.localNormalAt(Tuple.point(10, 0, -10)))
+        assertAlmostEquals(Tuple.VY, p.localNormalAt(Tuple.point(-5, 0, 150)))
+    }
+
+    @Test
+    fun `Intersect with ray parallel to plane`() {
+        val p = Plane()
+        val r = Ray(Tuple.point(0, 10, 0), Tuple.vector(0, 0, 1))
+        val xs = p.localIntersect(r)
+        assertTrue(xs.isEmpty())
+    }
+
+    @Test
+    fun `Intersect with coplanar ray`() {
+        val p = Plane()
+        val r = Ray(Tuple.PZERO, Tuple.VZ)
+        val xs = p.localIntersect(r)
+        assertTrue(xs.isEmpty())
+    }
+
+    @Test
+    fun `Ray intersection from above`() {
+        val p = Plane()
+        val r = Ray(Tuple.PY, Tuple.vector(0, -1, 0))
+        val xs = p.localIntersect(r)
+        assertEquals(1, xs.size)
+        assertAlmostEquals(1, xs[0].t)
+        assertEquals(p, xs[0].shape)
+    }
+
+    @Test
+    fun `Ray intersection from below`() {
+        val p = Plane()
+        val r = Ray(Tuple.point(0, -1, 0), Tuple.VY)
+        val xs = p.localIntersect(r)
+        assertEquals(1, xs.size)
+        assertAlmostEquals(1, xs[0].t)
+        assertEquals(p, xs[0].shape)
+    }
+}
--- src/test/kotlin/shapes/spheretest.kt
+++ src/test/kotlin/shapes/spheretest.kt
@@ -85,22 +85,6 @@ class SphereTest {
     }
 
     @Test
-    fun `Ray intersects scaled sphere`() {
-        val r = Ray(Tuple.point(0, 0, -5), Tuple.VZ)
-        val s = Sphere(Matrix.scale(2, 2, 2))
-        val xs = s.intersect(r)
-        assertAlmostEquals(listOf(3.0, 7.0), xs.map(Intersection::t))
-    }
-
-    @Test
-    fun `Ray intersects translated sphere`() {
-        val r = Ray(Tuple.point(0, 0, -5), Tuple.VZ)
-        val s = Sphere(Matrix.translate(5, 0, 0))
-        val xs = s.intersect(r)
-        assertTrue(xs.isEmpty())
-    }
-
-    @Test
     fun `Normal on sphere at point on the x axis`() {
         val s = Sphere()
         val nv = s.normalAt(Tuple.PX)
@@ -135,21 +119,5 @@ class SphereTest {
         val rt3by3 = sqrt(3.0) / 3
         val nv = s.normalAt(Tuple.point(rt3by3, rt3by3, rt3by3))
         assertAlmostEquals(nv.normalized, nv)
-    }
-
-    @Test
-    fun `Normal on a translated sphere`() {
-        val s = Sphere(Matrix.translate(0, 1, 0))
-        val value = 0.70711
-        val nv = s.normalAt(Tuple.point(0, 1 + value, -value))
-        assertAlmostEquals(Tuple.vector(0, value, -value), nv)
-    }
-
-    @Test
-    fun `Normal on a transformed sphere`() {
-        val s = Sphere(Matrix.rotationZ(PI/5).andThen(Matrix.scale(1, 0.5, 1)))
-        val rt2by2 = sqrt(2.0) / 2
-        val nv = s.normalAt(Tuple.point(0, rt2by2, -rt2by2))
-        assertAlmostEquals(Tuple.vector(0, 0.97014, -0.24254), nv)
     }
 }
