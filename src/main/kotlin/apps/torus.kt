bc5de852c5e16be5a0d55bcffcc20097ea504af8
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-01-31 17:58:13 -1000

    Torus. Finally happy with first scene.

--- src/main/kotlin/apps/torus.kt
+++ src/main/kotlin/apps/torus.kt
@@ -10,6 +10,7 @@ import math.Tuple
 import pattern.CheckerPattern
 import scene.Camera
 import scene.World
+import shapes.Cube
 import shapes.Plane
 import shapes.Torus
 import java.io.File
@@ -18,7 +19,10 @@ import kotlin.system.measureTimeMillis
 
 fun main() {
     val cube = run {
-        val t = Matrix.translate(0, 1.5, 0) * Matrix.scale(2, 4, 8)
+        val t = Matrix.scale(6, 4, 8)
+        val p = CheckerPattern(Color(0.9, 0.4, 0.4), Color.WHITE, Matrix.scale(0.5, 0.5, 0.5))
+        val m = Material(p, specular = 0)
+        Cube(t, m)
     }
 
     val plane = run {
@@ -28,18 +32,19 @@ fun main() {
     }
 
     val torus2 = run {
-        val t = Matrix.translate(0, 0, 2) * Matrix.rotateX(PI / 2)
+//        val t = Matrix.translate(0, 0, 2) * Matrix.rotateX(PI / 2)
+        val t = Matrix.translate(0, -1.5, 3) * Matrix.rotateX(PI / 2)
         Torus(2, 0.5, t)
     }
 
     val world = run {
-        val light = PointLight(Tuple.point(0, 10, -5))
-        World(listOf(plane, torus2), listOf(light))
+        val light = PointLight(Tuple.point(-1, 3.5, -1))
+        World(listOf(cube, torus2), listOf(light))
     }
 
     val camera = run {
-        val from = Tuple.point(0, 0, -5)
-        val to = Tuple.PZERO
+        val from = Tuple.point(0, -1.5, -4)
+        val to = Tuple.point(0, -1.5, 0)
         val t = from.viewTransformationFrom(to, Tuple.VY)
         Camera(1000, 1000, 1, t)
     }
