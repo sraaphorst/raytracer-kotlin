402c46f850b225db38a712dac2b50dde1bc27adf
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-01-31 16:14:04 -1000

    Torus: scenes adjusted and made. Code still needs cleanup.

--- src/main/kotlin/apps/torus.kt
+++ src/main/kotlin/apps/torus.kt
@@ -17,43 +17,31 @@ import kotlin.math.PI
 import kotlin.system.measureTimeMillis
 
 fun main() {
+    val cube = run {
+        val t = Matrix.translate(0, 1.5, 0) * Matrix.scale(2, 4, 8)
+    }
+
     val plane = run {
         val t = Matrix.translate(0, -2.5, 0)
         val m = Material(CheckerPattern(Color(0.9, 0.4, 0.4), Color.WHITE), specular = 0)
         Plane(t, m)
     }
 
-    val torus = run {
-        val m = Material(Color(0.9, 0.9, 0.9), reflectivity = 0.9)
-        val t = Matrix.translate(5, 1, 2)
-        Torus(2, 0.5, t)
-    }
-
     val torus2 = run {
-//        val m = Material(Color(0.9, 0.9, 0.9), reflectivity = 0.9)
         val t = Matrix.translate(0, 0, 2) * Matrix.rotateX(PI / 2)
         Torus(2, 0.5, t)
     }
 
     val world = run {
-//        val light = PointLight(Tuple.point(0, 5, 3))
         val light = PointLight(Tuple.point(0, 10, -5))
         World(listOf(plane, torus2), listOf(light))
     }
 
-//    val camera = run {
-//        val from = Tuple.point(0, 2, -1)
-//        val to = Tuple.PZERO
-//        val up = Tuple.VY
-//        val t = from.viewTransformationFrom(to, up)
-//        Camera(200, 50, PI / 2, t)
-//    }
-
     val camera = run {
-        val from = Tuple.point(2, 3, -5)
+        val from = Tuple.point(0, 0, -5)
         val to = Tuple.PZERO
         val t = from.viewTransformationFrom(to, Tuple.VY)
-        Camera(500, 500, 1, t)
+        Camera(1000, 1000, 1, t)
     }
 
     val elapsed = measureTimeMillis {
